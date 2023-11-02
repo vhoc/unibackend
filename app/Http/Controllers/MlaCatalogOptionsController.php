1a7a426e68a6207bc2ea59b4d0ebf0b8d5ab847c
Víctor Olvera <vhocar@gmail.com>
2023-11-01 19:38:43 -0600

    use original filename

--- app/Http/Controllers/MlaCatalogOptionsController.php
+++ app/Http/Controllers/MlaCatalogOptionsController.php
@@ -48,7 +48,7 @@ class MlaCatalogOptionsController extends Controller
                 // Store the new uploaded image.
                 Storage::disk('public')->makeDirectory('uploads/' . strval($userId) );
                 try {
-                    $path = $request->file('file')->storeAs('public/uploads/' . strval($userId) . '/' . 'hero-image.jpg');
+                    $path = $request->file('file')->storeAs('public/uploads/' . strval($userId) . '/' . $originalFileName);
                 } catch (\Throwable $e) {
                     return response([
                         "status" => $e->getCode(),
@@ -62,11 +62,11 @@ class MlaCatalogOptionsController extends Controller
                 // If an image assossiated with a userID exists, update the URL in the database,
                 if ( $userOptions ) {
                     // We have to delete the image first before in the DB, otherwise we lose the current filename.
-                    if ( Storage::disk('public')->exists('uploads/' . strval($userId) . '/' . 'hero-image.jpg') ) {
-                        Storage::disk('public')->delete('uploads/' . strval($userId) . '/' . 'hero-image.jpg');
+                    if ( Storage::disk('public')->exists('uploads/' . strval($userId) . '/' . $originalFileName) ) {
+                        Storage::disk('public')->delete('uploads/' . strval($userId) . '/' . $originalFileName);
                     }
 
-                    $userOptions->heading_image_url = env("FRONTEND_URL") . '/uploads/' . strval($userId) . '/' . 'hero-image.jpg';
+                    $userOptions->heading_image_url = env("FRONTEND_URL") . '/uploads/' . strval($userId) . '/' . $originalFileName;
                     $userOptions->save();
                     
                 // if not, create a new record.
