c96d93e609c90b5f8dd38853a3c5e7fc872171a9
Víctor Olvera <vhocar@gmail.com>
2023-11-02 19:03:02 -0600

    Added hexId to user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('hexId');
             $table->string('name');
             $table->string('email')->unique();
             $table->unsignedBigInteger('ecwidUserId')->nullable();
